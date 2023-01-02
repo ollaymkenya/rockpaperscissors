1f3a4ce27082db85ed19fb0990b7c9f9e66577ce
ollaymkenya <muriithioliver11@outlook.com>
2023-01-02 14:16:14 +0300

    This will add transition in opacity of images

--- script.js
+++ script.js
@@ -1,116 +1,101 @@
-// what happens
-const choices = [
-  { imageUrl: './assets/rock-person.png', name: 'rock' },
-  { imageUrl: './assets/paper-person.png', name: 'paper' },
-  { imageUrl: './assets/scissors-person.png', name: 'scissors' },
-];
+var choices = ['rock', 'paper', 'scissors'];
 
-var optionsContainer = document.querySelector('.options');
-var options = document.querySelectorAll('.option');
-var verdictContainer = document.querySelector('.verdict');
 var main = document.querySelector('main');
-var possibleAnswersPerson = document.querySelectorAll(
-  '.possible-answers__person img'
-);
-var possibleAnswersComputer = document.querySelectorAll(
-  '.possible-answers__computer img'
-);
+var verdictContainer = document.querySelector('.verdict');
+var personChoice = document.querySelector('#person');
+var computerChoice = document.querySelector('#computer');
+var possiblePersonChoices = [
+  ...document.querySelectorAll('.possibe-choice__person'),
+];
+var possibleComputerChoices = [
+  ...document.querySelectorAll('.possibe-choice__computer'),
+];
+var optionsContainer = document.querySelector('.options');
+var options = [...document.querySelectorAll('.option')];
 
-function getDecision(decisionMaker, computerChoice, personChoice) {
-  var verdict = {};
+function verdictHandler(personChoice, computerChoice) {
+  // personChoice - computerChoice == 1 || -2
+  var won =
+    personChoice - computerChoice === 1 || personChoice - computerChoice === -2
+      ? true
+      : personChoice - computerChoice === 0
+      ? null
+      : false;
 
-  //   making the decision
-  if (decisionMaker) {
+  var verdict;
+  if (won) {
     verdict = {
       color: 'green',
-      answer: `You have won: ${personChoice} beats ${computerChoice}`,
+      value: `You have won: ${choices[personChoice]} beats ${choices[computerChoice]}`,
     };
-  } else if (decisionMaker === null) {
+  } else if (won === null) {
     verdict = {
       color: 'orange',
-      answer: `It's a draw on: ${personChoice}`,
+      value: `It's a tie on: ${choices[personChoice]}`,
     };
   } else {
     verdict = {
       color: 'brown',
-      answer: `You have lost: ${computerChoice} beats ${personChoice}`,
+      value: `You have lost: ${choices[computerChoice]} beats ${choices[personChoice]}`,
     };
   }
 
   return verdict;
 }
 
-function getVerdict(computerIndex, personIndex) {
-  // our logic
-  var difference = computerIndex - personIndex;
-
-  // getting the choices
-  var computerChoice = choices[computerIndex].name;
-  var personChoice = choices[personIndex].name;
-
-  //   main logic
-  var decisionMaker =
-    difference === 0
-      ? null
-      : difference === -1 || difference === 2
-      ? true
-      : false;
-
-  return getDecision(decisionMaker, computerChoice, personChoice);
-}
-
-function changeImages(index, arrOfPlayers) {
-  arrOfPlayers.forEach((player) => {
-    player.forEach((possibleAnswer, possibleAnswerIndex) => {
-      possibleAnswer.dataset.possible = false;
-      if (possibleAnswerIndex === index) possibleAnswer.dataset.possible = true;
+function imageHandler(index, choices) {
+  choices.map((choice) => {
+    choice.map((img, idx) => {
+      img.dataset.visible = 'false';
+      if (idx === index) img.dataset.visible = 'true';
     });
   });
 }
 
-async function animateChoosing() {
-  var index = 0;
-  return await new Promise((resolve) => {
+async function animateImages() {
+  let index = 0;
+  await new Promise((resolve) => {
     let interval = setInterval(() => {
       if (index < 3) {
-        // changing background images
-        changeImages(index, [possibleAnswersPerson, possibleAnswersComputer]);
+        imageHandler(index, [possiblePersonChoices, possibleComputerChoices]);
+      }
+      if (index === 3) {
+        clearInterval(interval);
+        resolve();
       }
       index += 1;
-      if (index === 4) resolve(clearInterval(interval));
     }, 500);
   });
-  //   changing the background
 }
 
-options.forEach((inputChoice) => {
-  inputChoice.addEventListener('click', async (ev) => {
-    changeImages(3, [possibleAnswersPerson, possibleAnswersComputer]);
+options.map((option) => {
+  option.addEventListener('click', async (evt) => {
+    // removing the previously chosen option
+    imageHandler(3, [possiblePersonChoices, possibleComputerChoices]);
 
-    // remove the possibility to see the choices
     optionsContainer.style.display = 'none';
 
-    // generating the choice for the computer and getting your choice.
-    var randomNumber = Math.floor(Math.random() * 3);
-    var computerChoice = randomNumber === 3 ? 2 : randomNumber;
-    var personChoice = parseInt(ev.target.dataset.value);
+    // person's choice in number form
+    var psnChoice = parseInt(evt.target.dataset.value);
 
-    // choising the verdict
-    var verdict = getVerdict(computerChoice, personChoice);
+    // pecomputer's choice in number form
+    var randomChoice = Math.floor(Math.random() * 3);
+    var compChoice = randomChoice < 3 ? randomChoice : 2;
 
-    // animation of computer and person choosing
-    const promise = await animateChoosing();
+    // verdict = {color, value}
+    var verdict = verdictHandler(psnChoice, compChoice);
 
-    // showing what you both chose
-    changeImages(personChoice, [possibleAnswersPerson]);
-    changeImages(computerChoice, [possibleAnswersComputer]);
+    // animation
+    await animateImages();
 
-    // showing the answer
-    verdictContainer.innerHTML = `<p style="color: ${verdict.color}; margin: 0 auto;">${verdict.answer}</p>`;
+    // changin the verdict on the top
+    verdictContainer.innerHTML = `<p style="color: ${verdict.color}">${verdict.value}</p>`;
 
-    // adding the possibility to see the choices
-    optionsContainer.style.display = 'flex';
+    // gettign the final images presented
+    imageHandler(psnChoice, [possiblePersonChoices]);
+    imageHandler(compChoice, [possibleComputerChoices]);
+
     main.style.background = verdict.color;
-    return;
+    optionsContainer.style.display = 'flex';
   });
 });
